58f8606ce8def79abd17ad1647da2a38c42b6b4c
emelbee <35521179+emelbee@users.noreply.github.com>
2020-11-06 15:59:55 +0100

    Create onboardapplication.ps1

---
+++ onboardapplication.ps1
@@ -0,0 +1,80 @@
+########################################
+# now onboard the applicatione     #
+########################################
+# Define the FQDN for the REST APIs
+$FQDN = 'https://comp01.cybr.com'
+
+# get the api logon credentials
+# here we will use the dap integration
+# to retrieve the api credentials
+
+# We got the creds for the REST APIs so we are good to go!
+ "$(Get-Date) Credentials retrieved, logging in to REST APIs"
+  
+
+
+$logonInfo = @{}
+
+  #$logonInfo.username = $username
+  #$logonInfo.password = $password
+  $logonInfo.username = "dapprovisioning"
+  $logonInfo.password = "Cyberark1"
+
+#inputparameters
+
+$applicationpassword = "P@ssW0rD!"
+$applicationaddress =  "dbservername"
+$applicationusername = "dbappuser"
+$applicationsafe = "application1"
+$applicationplatform = "AzureWindowsServerAccounts"
+
+
+ "$(Get-Date) test input done "
+# "$(Get-Date) the input used is $username $targetpassword $targetaddress"
+ 
+
+##########################################################
+  # Use REST APIs to logon to the CyberArk Vault
+  ##########################################################
+  $loginURI = $FQDN + '/PasswordVault/WebServices/auth/cyberark/CyberArkAuthenticationService.svc/logon'
+  #login to the Vault
+  $result = Invoke-RestMethod -Method Post -Uri $loginURI -ContentType "application/json" -Body (ConvertTo-Json($logonInfo))
+  "$(Get-Date) Vault login successful"
+  
+  $logonToken = $result.CyberArkLogonResult
+  # Define the Account Management URL
+  $createAccountURI = $FQDN + '/PasswordVault/WebServices/PIMServices.svc/Account'
+  # Account parameters
+  $newAccounts = @{}
+  $newAccount = @{}
+  $newAccount.safe = "Azure"
+  $newAccount.platformID = $applicationplatform
+
+  #$newAccount.address = $FullyQualifiedDomainName
+  $newAccount.address = "$targetaddress"
+  # The default Windows account is always Administrator...
+  $newAccount.username = $username
+  $newAccount.password = $targetpassword
+  $newAccount.accountName = $instanceid
+  # Add the account to create to the accounts array
+  $newAccounts.account = $newAccount
+  # Set the authorisation token in the headers for the REST call
+  $headers = @{ "Authorization" = $logonToken }
+  ##########################################################
+  # Use REST APIs to create the account in the CyberArk Vault
+  ##########################################################
+  $result = Invoke-RestMethod -Method Post -Uri $createAccountURI -headers $headers -ContentType "application/json" -Body (ConvertTo-Json($newAccounts))
+  "$(Get-Date) Account created successfully" 
+  ##########################################################
+  # Use REST APIs to logoff from the Vault
+  ##########################################################
+  $logoffURI = $FQDN + '/PasswordVault/WebServices/auth/cyberark/CyberArkAuthenticationService.svc/logoff'
+  $result = Invoke-RestMethod -Method Post -Uri $logoffURI -headers $headers -ContentType "application/json" -Body (ConvertTo-Json($logonInfo))
+  "$(Get-Date) Vault logoff successful"
+ # "$(Get-Date) Finished provisioning instance in AWS and the privileged account of instance $instanceid, with IP address $publicIP in the CyberArk Vault"
+ # pause
+  # Clear AWS credentials so no PowerShell script in the same context can access them
+ # Remove-AWSCredentialProfile -ProfileName default -Force
+  # This will delete the only way you can decrypt the password using AWS provided capabilities
+ # Remove-Item "$KeyFilePath"
+#}
